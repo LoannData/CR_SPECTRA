fd11cbd7caf5e79feff68d4400c624446ca01e5d
Loann <loann.datasciences@gmail.com>
2019-05-28 10:39:50 +0200

    Add new file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+FILE     : SETUP.PY 
+FONCTION : Generate the folder of work, this folder will contain the initial values, 
+           the properties of the simulation, the output data and the compiled source code  
+
+"""
+
+import os 
+import sys 
+
+filename = "test" 
+
+os.mkdir(filename)
