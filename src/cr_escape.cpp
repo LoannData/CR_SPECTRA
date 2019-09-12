e64f7285c096679102867d6c3cdb6de38d1b84d3
Loann <loann.datasciences@gmail.com>
2019-09-12 10:45:15 +0200

    cr_escape modif 2. (not finished!)

--- src/cr_escape.cpp
+++ src/cr_escape.cpp
@@ -201,15 +201,107 @@ double InterpolatingSpline(vector<double> X, vector<double> Y, double x)
         return loc_a + loc_b + loc_c;
     }
 
+
+
+
+
+
+
 int main()
     {
-        vector<double> X = {1, 2, 3, 4, 5};
-        vector<double> Y = {1, 2, 5, 4, 5};
-        double x = 2.6; 
 
-        double y = InterpolatingSpline(X, Y, x);
+// Main constants 
+double pi = 3.14720;
+double c  = 2.997e10; 
+double yr = 362.25*86400;
+double kyr = yr*1e3;
+double pc   = 3.086e18;   // 1 pc in [cm]
 
-        cout<<"y = "<<y<<endl;
+// We define the constants of our problem
+double nt    = 0.35;  // [atom/cm^-3] Density, here WNM
+double xhi_m = 1; // Metallicity (1 for solar abundances)
+double xhi_cr= 0.1;
+double E51   = 1; // [10^51 erg] Total energy released by the SNR
+double Mej   = 1; // [Msum] Total mass released by the SNR
+double C06   = 1; // 
+double beta  = 1; //
+double phi_c = 1; // Ratio of the thermal conductivity to the Spitzer (1962) value
+double vej8  = 10.*pow(E51/Mej, 0.5);
+
+//###############################################################################
+//# FUNCTIONS IN ORDER TO MAKE OUR SNR EXPAND IN THE ISM                        #
+//###############################################################################
+//# We define the characteristic times of our problem
+double tini   = 1e-4*kyr; // [s]
+double tfree  = 0.3*pow(E51,-0.5)*Mej*pow(nt,-1./3)*kyr; // [s]
+double tPDS   = exp(-1.)*3.61e4*pow(E51,3./14)/(pow(xhi_m,5./14)*pow(nt,4./7))*yr; // [s]
+double tMCS   = min(61*pow(vej8,3)/(pow(xhi_m,9./14)*pow(nt,3./7)*pow(E51,3./14)), 476./pow(xhi_m*phi_c,9./14))*tPDS; // [s]
+double tmerge = 153.*pow(pow(E51,1./14)*pow(nt,1./7)*pow(xhi_m,3./14)/(beta*C06), 10./7)*tPDS; // [s]
+double tmax = min(tMCS, tmerge); // [s]
+
+
+// We define the characteristic radii of our problem
+vector<double> t;
+vector<double> r;
+double R_free  = 5.0*pow(E51/nt,1./5)*pow(1 - (0.05*pow(Mej,5./6))/(pow(E51,0.5)*pow(nt,1./3)*(tfree/kyr)), 2./5)*pow(tfree/kyr,2./5)*pc; // [cm]
+double R_ini   = R_free*(tini/tfree); // [cm]
+double R_PDS   = 5.0*pow(E51/nt,1./5)*pow(1 - (0.05*pow(Mej,5./6))/(pow(E51,0.5)*pow(nt,1./3)*(tPDS/kyr)), 2./5)*pow(tPDS/kyr,2./5)*pc; // [cm]
+double R_MCS   = R_PDS*pow(tMCS/tPDS, 3./10); // [cm]
+double R_merge = R_MCS*pow(tmerge/tMCS, 1./4); // [cm]
+if (tMCS < tmerge)
+{
+    t = {tini, tfree, tPDS, tMCS, tmerge};
+    r = {R_ini, R_free, R_PDS, R_MCS, R_merge};
+}
+if (tMCS >= tmerge)
+{
+    t = {tini, tfree, tPDS, tmerge};
+    r = {R_ini, R_free, R_PDS, R_merge};
+}
+
+vector<double> logt; logt.resize(t.size());
+vector<double> logr; logr.resize(r.size());
+for (int i = 0; i < logt.size(); i++)
+{
+    logt[i] = log10(t[i]);
+    logr[i] = log10(r[i]);
+}
+
+int loc_size = 100;
+vector<double> logt_new; logt_new.resize(loc_size);
+vector<double> logr_new; logr_new.resize(loc_size);
+for (int i = 0; i < loc_size; i++)
+{
+    logt_new[i] = logt[0] + i*(logt[logt.size()-1] - logt[0])/loc_size;
+    logr_new[i] = InterpolatingSpline(logt, logr, logt_new[i]);
+}
+
+vector<double> t_new; t_new.resize(logt_new.size());
+vector<double> r_new; r_new.resize(logr_new.size());
+for (int i = 0; i < loc_size; i++)
+{
+    t_new[i] = pow(10, logt_new[i]);
+    r_new[i] = pow(10, logr_new[i]);
+}
+
+// We calculate the shock velocity 
+vector<double> u_sh; u_sh.resize(t_new.size());
+for (int i = 1; i < u_sh.size(); i++)
+    {
+        u_sh[i] = (r_new[i] - r_new[i-1])/(t_new[i] - t_new[i-1]);
+    }
+u_sh[0] = u_sh[1] - (u_sh[2] - u_sh[1]);
+
+
+
+
+
+
+
+
+
+
+
 
 
     return 0;    
