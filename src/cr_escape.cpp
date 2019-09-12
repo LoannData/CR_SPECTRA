140729bc902e4b95876929752074db2975aa8cb6
Loann <loann.datasciences@gmail.com>
2019-09-12 09:06:32 +0200

    Begining cr_escape model

---
+++ src/cr_escape.cpp
@@ -0,0 +1,216 @@
+#include <iostream> //For console output/input
+#include <fstream>  //Allows to read/write files 
+#include <math.h>   //Basic mathematic functions 
+#include <vector>   //For dynamic arrays 
+#include <string>   //Operations on strings 
+#include <tuple>
+#include <cmath>
+#include <sstream>
+using namespace std; 
+
+
+// TriDiagonal matrix inversion function
+void InverseTrigonalMatrix(vector<vector<double>> &T)
+    {
+    int n = T.size();
+
+    
+
+    double a[n];
+    double b[n];
+    double c[n];
+    for (int i = 0; i < n; i++)
+        {
+            b[i] = T[i][i];
+            if (i < n-1){c[i] = T[i][i+1];}
+            if (i > 0){a[i] = T[i][i-1];}
+        }
+    
+
+    double theta_mm = 0.;
+    double theta_m = 1.;
+    double theta[n];
+    theta[0] = b[0]*theta_m;
+    theta[1] = b[1]*theta[0] - a[1]*c[0]*theta_m;
+    for (int i = 2; i < n; i++)
+        {
+            theta[i] = b[i]*theta[i-1] - a[i]*c[i-1]*theta[i-2];
+        }
+    
+    double phi_pp = 0;
+    double phi_p  = 1;
+    double phi[n+2];
+    phi[n] = phi_p;
+    phi[n+1] = phi_pp;
+    phi[n-1] = b[n-1]*phi_p;
+    phi[n-2] = b[n-2]*phi[n-1] - c[n-2]*a[n-1]*phi_p;
+    for (int i = n-3; i > -1; i--)
+        {
+            phi[i] = b[i]*phi[i+1] - c[i]*a[i+1]*phi[i+2];
+        }
+    
+    vector<vector<double>> Tinv;
+    double Tij;
+    Tinv.resize(n);
+    for (int i = 0; i < n; i++)
+        {
+            
+            Tinv[i].resize(n);
+            for (int j = 0; j < n; j++)
+                {
+                    //cout<<i<<", "<<j<<endl;
+                    if (i < j)
+                        {
+                            double p = 1.;
+                            double loc_theta;
+                            for (int ei = i; ei < j; ei++)
+                                {
+                                    p = p*c[ei];
+                                }
+                            if (i-1 == -1){loc_theta = theta_m;}
+                            if (i-1 > 1){loc_theta = theta[i-1];}
+                            Tij = pow(-1,i+j)*p*loc_theta*phi[j+1]/theta[n-1];
+                        }
+                    if (i == j)
+                        {
+                            double loc_theta;
+                            if (i-1 == -1){loc_theta = theta_m;}
+                            if (i-1 > -1){loc_theta = theta[i-1];}
+                            Tij = loc_theta*phi[j+1]/theta[n-1];
+                        }
+                    if (i > j)
+                        {
+                            double p = 1.;
+                            double loc_theta;
+                            for (int ei = j+1; ei < i+1; ei++)
+                                {
+                                    p = p*a[ei];
+                                }
+                            
+                            if (j-1 == -1){loc_theta = theta_m;}
+                            if (j-1 > -1){loc_theta = theta[j-1];}
+                            
+                            Tij = pow(-1, i+j)*p*loc_theta*phi[i+1]/theta[n-1];
+                            //cout<<"Hello"<<endl;
+                        }
+                    Tinv[i][j] = Tij;
+                }
+        }
+        T = Tinv;
+    }
+
+void ProductMatrix(vector<vector<double>> A, vector<vector<double>> B, vector<vector<double>> &C)
+    {
+        int A_l = A.size();
+        int A_c = A[0].size();
+
+        int B_l = B.size();
+        int B_c = B[0].size();
+
+        int C_l = A_l;
+        int C_c = B_c;
+
+        
+        C.resize(C_l);
+        for (int i = 0; i < C_l; i++)
+            {
+                C[i].resize(C_c);
+                for (int j = 0; j < C_c; j++)
+                {
+                    double s = 0.;
+                    for (int k = 0; k < A_c; k++)
+                        {
+                            s = s + A[i][k]*B[k][j];
+                        }
+                    C[i][j] = s;
+                }
+            }
+    }
+
+double InterpolatingSpline(vector<double> X, vector<double> Y, double x)
+    {
+        int N = X.size();
+
+        
+        double h[N-1];
+        for (int i = 0; i < N-1; i++)
+            {
+                h[i] = X[i+1] - X[i];
+            }
+        
+        
+        double lowF = 0.;
+        double highF = 0.;
+        vector<vector<double>> F; 
+        F.resize(N);
+        for (int i = 0; i < N; i++)
+            {
+                F[i].resize(1);
+            }
+        F[0][0] = lowF;
+        for (int i = 1; i < N-1; i++)
+            {
+                F[i][0] = (Y[i+1] - Y[i])/h[i] - (Y[i] - Y[i-1])/h[i-1];
+            }
+        F[N-1][0] = highF;
+
+        
+        vector<vector<double>> R;
+        R.resize(N);
+        for (int i = 0; i < N; i++)
+            {
+                R[i].resize(N);
+            }
+        R[0][0] = 1.;
+        R[N-1][N-1] = 1.;
+        for (int i = 1; i < N-1; i++)
+            {
+                R[i][i] = (h[i-1] + h[i])/3.;
+                R[i][i+1] = h[i]/6.;
+                R[i][i-1] = h[i-1]/6.;
+            }
+        
+        
+        vector<vector<double>> Rinv;
+        InverseTrigonalMatrix(R);
+        Rinv = R;
+
+        vector<vector<double>> M; 
+        
+        ProductMatrix(Rinv, F, M);
+        
+        double Ci[N-1]; 
+        double Cip[N-1];
+        for (int i = 0; i < N-1; i++)
+            {
+                Ci[i]  = (Y[i+1] - Y[i])/h[i] - h[i]*(M[i+1][0] - M[i][0])/6.;
+                Cip[i] = Y[i] - M[i][0]*pow(h[i],2)/6.;
+            }
+        
+        double loc_a, loc_b, loc_c;
+        for (int k = 0; k < N-1; k++)
+            {
+                if (x >= X[k] and x <= X[k+1])
+                    {
+                        loc_a = M[k][0]*pow(X[k+1] - x,3)/(6*h[k]);
+                        loc_b = M[k+1][0]*pow(x - X[k], 3)/(6*h[k]);
+                        loc_c = Ci[k]*(x-X[k]) + Cip[k];
+                    }
+            }
+        
+        return loc_a + loc_b + loc_c;
+    }
+
+int main()
+    {
+        vector<double> X = {1, 2, 3, 4, 5};
+        vector<double> Y = {1, 2, 5, 4, 5};
+        double x = 2.6; 
+
+        double y = InterpolatingSpline(X, Y, x);
+
+        cout<<"y = "<<y<<endl;
+
+
+    return 0;    
+    }
